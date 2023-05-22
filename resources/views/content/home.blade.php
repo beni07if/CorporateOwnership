7e2e700f18728ce555812484f337798bd5f4e980
Beni <beni07if@gmail.com>
2023-05-22 13:31:56 +0700

    switch position group etc

--- resources/views/content/home.blade.php
+++ resources/views/content/home.blade.php
@@ -83,10 +83,10 @@
                 <div class="col-lg-3">
                     <ul class="nav nav-tabs flex-column">
                         <li class="nav-item">
-                            <a class="nav-link active show" data-bs-toggle="tab" href="#tab-1">Group of company</a>
+                            <a class="nav-link " data-bs-toggle="tab" href="#tab-1">Group of company</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" data-bs-toggle="tab" href="#tab-2">Subsidiary</a>
+                        <li class="nav-item ">
+                            <a class="nav-link active show" data-bs-toggle="tab" href="#tab-2">Subsidiary</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" data-bs-toggle="tab" href="#tab-3">Company shareholders</a>
@@ -95,7 +95,7 @@
                 </div>
                 <div class="col-lg-9 mt-4 mt-lg-0">
                     <div class="tab-content">
-                        <div class="tab-pane active show" id="tab-1">
+                        <div class="tab-pane " id="tab-1">
                             <div class="row">
                                 <div class="col-lg-8 details order-2 order-lg-1">
                                     <h3>Group of company</h3>
@@ -123,7 +123,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="tab-pane chatbox" id="tab-2">
+                        <div class="tab-pane active show chatbox" id="tab-2">
                             <div class="row">
                                 <div class="col-lg-8 details order-2 order-lg-1">
                                     <h3>Subsidiary</h3>
