513c4910dfd32609a7be98109924ceaf255ecc6c
Beni <beni07if@gmail.com>
2023-05-22 13:00:09 +0700

    logo eq

--- resources/views/content/home.blade.php
+++ resources/views/content/home.blade.php
@@ -175,13 +175,13 @@
         </div>
     </section><!-- End Departments Section -->
 
-    <!-- ======= About Section ======= -->
+    <!-- ======= About Sections ======= -->
     <section id="about" class="about">
         <div class="container-fluid">
 
             <div class="row">
                 <div class="col-xl-5 col-lg-6 video-box d-flex justify-content-center align-items-stretch position-relative">
-                    <img src="{{ asset('img/EQ.PNG') }}" alt="" class="img-fluid">
+                    <img src="{{ asset('img/EQ.png') }}" alt="" class="img-fluid">
                 </div>
 
                 <div class="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5">
