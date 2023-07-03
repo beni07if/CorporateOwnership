cb7ed58d765a0e70adced2f029ecae157fd680f0
beni07if <beni07if@gmail.com>
2023-07-04 00:25:22 +0700

    update id

--- database/migrations/2023_03_06_031740_create_consolidations_table.php
+++ database/migrations/2023_03_06_031740_create_consolidations_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrationssss.
+     * Run the migrationssssss.
      */
     public function up(): void
     {
