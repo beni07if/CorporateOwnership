00a9f9510e12a88df4a2d1e7418d331e31815c2a
beni07if <beni07elreal@gmail.com>
2024-03-15 11:37:43 +0700

    delete price list section

---
+++ resources/views/content/standardPage.blade.php
@@ -0,0 +1,65 @@
+<section id="pricing" class="section" hidden>
+      <div class="container">
+
+        <div class="row justify-content-center text-center">
+          <div class="col-md-7 mb-5">
+            <h2 class="section-heading">PRICE LIST</h2>
+            <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere illum obcaecati inventore velit laborum earum.</p> -->
+          </div>
+        </div>
+        <div class="row align-items-stretch">
+
+          <div class="col-lg-4 mb-4 mb-lg-0">
+            <div class="pricing h-100 text-center">
+              <span>&nbsp;</span>
+              <h3>BASIC</h3>
+              <ul class="list-unstyled">
+                <li>Company Name</li>
+                <li>Activity</li>
+                <li>Location</li>
+              </ul>
+              <div class="price-cta">
+                <strong class="price">Free</strong>
+                <!-- <p><a href="#" class="btn btn-white">Choose Plan</a></p> -->
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 mb-4 mb-lg-0">
+            <div class="pricing h-100 text-center popular">
+              <span class="popularity">Recomended</span>
+              <h3>STANDARD</h3>
+              <ul class="list-unstyled">
+                <li>Company Type</li>
+                <li>Incorporation Date</li>
+                <li>Company Number</li>
+                <li>Date Company Number</li>
+                <li>Registered Address</li>
+                <li>Country of Registration</li>
+                <li>Business Address</li>
+                <li>Country of Business Address</li>
+                <li>Nature of Busiseness Address</li>
+                <li>Etc</li>
+              </ul>
+              <div class="price-cta">
+                <strong class="price">$5.000/years</strong>
+                <p><a href="#" class="btn btn-white">Choose Plan</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 mb-4 mb-lg-0">
+            <div class="pricing h-100 text-center">
+              <span class="popularity">Full Dataset</span>
+              <h3>PREMIUM</h3>
+              <ul class="list-unstyled">
+                <li>All in Standard</li>
+                <li>Mapping structure</li>
+              </ul>
+              <div class="price-cta">
+                <strong class="price">$10.000/years</strong>
+                <p><a href="#" class="btn btn-white">Choose Plan</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
