07448403374d7d2bbb32635add07507c13d047e4
beni07if <beni07elrel@gmail.com>
2024-08-14 14:35:38 +0700

    edit sidebar admin

--- resources/views/admin/layout/sidebarAdmin.blade.php
+++ resources/views/admin/layout/sidebarAdmin.blade.php
@@ -16,30 +16,6 @@
     </a>
   </li>
   <li class="nav-item">
-    <a href="{{route('groups.index')}}" class="nav-link collapsed {{ request()->is('inbox') ? 'active' : '' }}">
-      <i class="bi bi-inbox"></i>
-      <span>Group</span>
-    </a>
-  </li>
-  <li class="nav-item">
-    <a href="{{route('consolidations.index')}}" class="nav-link collapsed {{ request()->is('inbox') ? 'active' : '' }}">
-      <i class="bi bi-inbox"></i>
-      <span>Consolidation</span>
-    </a>
-  </li>
-  <li class="nav-item">
-    <a href="{{route('shareholders.index')}}" class="nav-link collapsed {{ request()->is('inbox') ? 'active' : '' }}">
-      <i class="bi bi-inbox"></i>
-      <span>Shareholder</span>
-    </a>
-  </li>
-  <li class="nav-item">
-    <a href="{{route('sras.index')}}" class="nav-link collapsed {{ request()->is('inbox') ? 'active' : '' }}">
-      <i class="bi bi-inbox"></i>
-      <span>SRA</span>
-    </a>
-  </li>
-  <li class="nav-item">
     <a href="{{route('faq.index')}}" class="nav-link collapsed {{ request()->is('inbox') ? 'active' : '' }}">
       <i class="bi bi-inbox"></i>
       <span>FAQ</span>
@@ -57,6 +33,37 @@
       <span>Term of Condition</span>
     </a>
   </li>
+  <li class="nav-item">
+    <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+      <i class="bi bi-menu-button-wide"></i><span>Database</span><i class="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <li class="nav-item">
+        <a href="{{route('groups.index')}}" class="nav-link collapsed {{ request()->is('inbox') ? 'active' : '' }}">
+          <i class="bi bi-inbox"></i>
+          <span>Group</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="{{route('consolidations.index')}}" class="nav-link collapsed {{ request()->is('inbox') ? 'active' : '' }}">
+          <i class="bi bi-inbox"></i>
+          <span>Consolidation</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="{{route('shareholders.index')}}" class="nav-link collapsed {{ request()->is('inbox') ? 'active' : '' }}">
+          <i class="bi bi-inbox"></i>
+          <span>Shareholder</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="{{route('sras.index')}}" class="nav-link collapsed {{ request()->is('inbox') ? 'active' : '' }}">
+          <i class="bi bi-inbox"></i>
+          <span>SRA</span>
+        </a>
+      </li>
+    </ul>
+  </li><!-- End Components Nav -->
   <!-- <li class="nav-item">
     <a href="{{route('messages.index')}}" class="nav-link collapsed {{ request()->is('inbox') ? 'active' : '' }}">
       <i class="bi bi-inbox"></i>
