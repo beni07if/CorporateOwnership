da14be154ce726fb131b6728ba93edd71d05ecf5
beni07if <beni07elrel@gmail.com>
2025-03-08 19:39:26 +0700

    hide login

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -17,7 +17,9 @@
     <link href="{{ asset('template/Medilab/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
 
@@ -36,19 +38,22 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aos@next/dist/aos.css" />
 
     <!-- Template Main CSS File -->
-    {{-- <link href="{{ asset('template/Medilab/assets/css/style.css') }}" rel="stylesheet"> --}}
+    {{--
+    <link href="{{ asset('template/Medilab/assets/css/style.css') }}" rel="stylesheet"> --}}
     <link href="{{ asset('template/Medilab/assets/css/styleFlexstate.css') }}" rel="stylesheet">
-    <link rel="icon" href="{!! asset('img/logo/new-logo-5/png/logo-icon.png') !!}"/>
-    {{-- <link href="{{ asset('css/style.css') }}" rel="stylesheet"> --}}
+    <link rel="icon" href="{!! asset('img/logo/new-logo-5/png/logo-icon.png') !!}" />
+    {{--
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet"> --}}
 
-    
 
 
-  <!-- Vendor CSS Files -->
 
-  <!-- Template Main CSS File -->
-  {{-- <link href="assets/css/style.css" rel="stylesheet"> --}}
-  <link href="{{ asset('template/Medilab/assets/css/style.css') }}" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+
+    <!-- Template Main CSS File -->
+    {{--
+    <link href="assets/css/style.css" rel="stylesheet"> --}}
+    <link href="{{ asset('template/Medilab/assets/css/style.css') }}" rel="stylesheet">
 
     <!-- <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" /> -->
     <!-- Make sure you put this AFTER Leaflet's CSS -->
@@ -65,21 +70,26 @@
 
     <style>
         [data-aos] {
-        opacity: 1 !important;
-        visibility: visible !important;
+            opacity: 1 !important;
+            visibility: visible !important;
         }
+
         #header {
-        background: rgba(240, 248, 255, 0); /* Transparan penuh */
-        transition: background-color 0.5s; /* Efek transisi */
-        z-index: 997;
-        padding: 15px 0;
-        box-shadow: 0px 2px 15px rgba(25, 119, 204, 0.1);
-        position: fixed;
-        width: 100%;
-        top: 0;
+            background: rgba(240, 248, 255, 0);
+            /* Transparan penuh */
+            transition: background-color 0.5s;
+            /* Efek transisi */
+            z-index: 997;
+            padding: 15px 0;
+            box-shadow: 0px 2px 15px rgba(25, 119, 204, 0.1);
+            position: fixed;
+            width: 100%;
+            top: 0;
         }
+
         .header-solid {
-        background: #fff !important; /* Warna putih */
+            background: #fff !important;
+            /* Warna putih */
         }
 
         #map {
@@ -88,14 +98,14 @@
         }
 
         .descriptions {
-        display: inline-block;
-        padding: 5px 5px;
-        font-size: 16px;
-        background-color: #f0f0f0;
-        border: none;
-        border-radius: 4px;
-        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-        transition: background-color 0.5s ease, transform 0.5s ease;
+            display: inline-block;
+            padding: 5px 5px;
+            font-size: 16px;
+            background-color: #f0f0f0;
+            border: none;
+            border-radius: 4px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            transition: background-color 0.5s ease, transform 0.5s ease;
         }
 
         .descriptions:hover {
@@ -136,7 +146,7 @@
             text-align: left;
         }
 
-            /* Gaya CSS untuk elemen input */
+        /* Gaya CSS untuk elemen input */
         .description {
             border: none;
             font-weight: bold;
@@ -145,85 +155,102 @@
 
         /* Efek hover: mengubah warna background menjadi abu-abu */
         form .text-muted:hover {
-            background-color: #dcdcdc; /* Warna abu-abu pada hover */
+            background-color: #dcdcdc;
+            /* Warna abu-abu pada hover */
         }
 
         .text-muted {
             border: none;
             background-color: transparent;
-            transition: background-color 0.3s; /* Efek transisi ketika hover */
+            transition: background-color 0.3s;
+            /* Efek transisi ketika hover */
             padding-bottom: 20px;
         }
 
         ul {
-        list-style: none; /* Menghilangkan simbol list (bulat kecil) */
-        padding: 0; /* Menghapus padding default untuk ul */
-        margin: 0; /* Menghapus margin default untuk ul */
+            list-style: none;
+            /* Menghilangkan simbol list (bulat kecil) */
+            padding: 0;
+            /* Menghapus padding default untuk ul */
+            margin: 0;
+            /* Menghapus margin default untuk ul */
         }
 
         ul li {
-            margin-bottom: 10px; /* Memberi jarak antara setiap elemen li */
+            margin-bottom: 10px;
+            /* Memberi jarak antara setiap elemen li */
         }
 
         .overlay {
-                    position: fixed;
-                    top: 0;
-                    left: 0;
-                    width: 100%;
-                    height: 100%;
-                    background-color: rgba(0, 0, 0, 0.8);
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    z-index: 1000;
-                }
-                .zoomed {
-                    max-width: 90%;
-                    max-height: 90%;
-                    transition: transform 0.5s ease;
-                }
-                .zoomed:hover {
-                    transform: scale(1.1);
-                }
-                .close-button {
-                    position: absolute;
-                    top: 20px;
-                    right: 20px;
-                    background-color: white;
-                    border: none;
-                    padding: 10px;
-                    cursor: pointer;
-                }
-                .overlay-card {
-                    background-color: white;
-                    cursor: pointer;
-                    position: absolute;
-                    top: 50%;
-                    left: 50%;
-                    transform: translate(-50%, -50%);
-                }
-                .gallery-item {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.8);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            z-index: 1000;
+        }
+
+        .zoomed {
+            max-width: 90%;
+            max-height: 90%;
+            transition: transform 0.5s ease;
+        }
+
+        .zoomed:hover {
+            transform: scale(1.1);
+        }
+
+        .close-button {
+            position: absolute;
+            top: 20px;
+            right: 20px;
+            background-color: white;
+            border: none;
+            padding: 10px;
+            cursor: pointer;
+        }
+
+        .overlay-card {
+            background-color: white;
+            cursor: pointer;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+
+        .gallery-item {
             margin: 10px;
             display: flex;
             justify-content: center;
             align-items: center;
-            height: 300px; /* Set a fixed height */
+            height: 300px;
+            /* Set a fixed height */
         }
+
         .gallery-image {
             object-fit: cover;
             width: 100%;
-            height: 100%; /* Set height to match the card */
+            height: 100%;
+            /* Set height to match the card */
         }
 
         /* Hover effect for button */
         .btn-info:hover {
-                background-color: #0056b3; /* Darker shade on hover */
-                color: white;
-            }
-            .zoom-in:hover {
-        color: #007BFF;
-        transform: scale(1.1); /* Zoom in effect */
-    }
+            background-color: #0056b3;
+            /* Darker shade on hover */
+            color: white;
+        }
+
+        .zoom-in:hover {
+            color: #007BFF;
+            transform: scale(1.1);
+            /* Zoom in effect */
+        }
 
         /* Tambahkan gaya lain sesuai kebutuhan Anda */
     </style>
@@ -243,12 +270,13 @@
     </div> -->
 
     <!-- ======= Header ======= -->
-    <header id="header" class="fixed-top" >
+    <header id="header" class="fixed-top">
         <div class="container d-flex align-items-center">
 
             <!-- <h1 class="logo me-auto"><a href="{{route('corporateProfileEn')}}">ID Corporate</a></h1> -->
             <!-- Uncomment below if you prefer to use an image logo -->
-            <a href="{{route('corporateProfileEn')}}" class="logo me-auto"><img src="{{asset('img/logo/new-logo/Agribiz_Color.png')}}" alt="" class="img-fluid"></a>
+            <a href="{{route('corporateProfileEn')}}" class="logo me-auto"><img
+                    src="{{asset('img/logo/new-logo/Agribiz_Color.png')}}" alt="" class="img-fluid"></a>
 
             <nav id="navbar" class="navbar order-last order-lg-0">
                 <ul>
@@ -275,32 +303,35 @@
                     <li><a class="" href="{{route('corporateProfileEn')}}#footer">Contact</a></li>
                     <li class="nav-item dropdown">
                         <!-- User icon and dropdown toggle -->
-                        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
                             <i class="fas fa-user"></i> <!-- Font Awesome User Icon -->
                         </a>
-                    
+
                         <!-- Dropdown menu -->
                         <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
                             @auth <!-- Check if the user is authenticated -->
-                            <li><span class="dropdown-item-text">{{ Auth::user()->name }}</span></li>
-                            <li><span class="dropdown-item-text">{{ Auth::user()->email }}</span></li>
-                            <li><hr class="dropdown-divider"></li>
-                            <li>
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                    Logout
-                                </a>
-                            </li>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
+                                <li><span class="dropdown-item-text">{{ Auth::user()->name }}</span></li>
+                                <li><span class="dropdown-item-text">{{ Auth::user()->email }}</span></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
+                                </li>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
                             @else <!-- If the user is not authenticated, show the contact link -->
-                            <li><a class="dropdown-item" href="{{ route('login') }}">Login</a></li>
+                                {{-- <li><a class="dropdown-item" href="{{ route('login') }}">Login</a></li> --}}
                             @endauth
                         </ul>
                     </li>
-                    
-                    
+
+
                 </ul>
                 <i class="bi bi-list mobile-nav-toggle"></i>
             </nav><!-- .navbar -->
@@ -324,7 +355,9 @@
             <div class="container">
                 <div class="row">
                     <div class="col-lg-6 col-md-3 footer-newsletter">
-                        <a href="{{route('corporateProfileEn')}}" class="logo me-auto"><img src="{{asset('img/logo/new-logo/Agribiz_Color.png')}}" alt="" class="img-fluid" style="height:40px; width: auto;"></a>
+                        <a href="{{route('corporateProfileEn')}}" class="logo me-auto"><img
+                                src="{{asset('img/logo/new-logo/Agribiz_Color.png')}}" alt="" class="img-fluid"
+                                style="height:40px; width: auto;"></a>
                         <br><br>
                         <ul>
                             <li><a href="{{route('faq')}}">FAQ (Frequently Asked Questions)</a></li>
@@ -335,9 +368,12 @@
                     <div class="col-lg-6 col-md-3 footer-newsletter">
                         <h4>Contact Us</h4>
                         <ul>
-                            <li><i class="bx bx-chevron-right"></i> Jl. Anggrek No.6, Pontianak City, West Kalimantan</li>
+                            <li><i class="bx bx-chevron-right"></i> Jl. Anggrek No.6, Pontianak City, West Kalimantan
+                            </li>
                             <li><i class="bx bx-chevron-right"></i> helpdesk@earthqualizer.org</li>
-                            {{-- <li><i class="bx bx-chevron-right"></i> <a href="https://wa.me/628982950531?text=Hello%20World" target="_blank">+628982950531</a></li> --}}
+                            {{-- <li><i class="bx bx-chevron-right"></i> <a
+                                    href="https://wa.me/628982950531?text=Hello%20World"
+                                    target="_blank">+628982950531</a></li> --}}
                         </ul>
                     </div>
 
@@ -370,7 +406,8 @@
     </footer><!-- End Footer -->
 
     <div id="preloader"></div>
-    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
 
     <!-- Vendor JS Files -->
     <script src="{{ asset('template/Medilab/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
@@ -383,29 +420,31 @@
     <script src="{{ asset('template/Medilab/assets/vendor/purecounter/purecounter_vanilla.js')}}"></script>
 
     <!-- Template Main JS File -->
-    {{-- <script src="assets/js/main.js"></script> --}}
-    {{-- <script src="{{ asset('template/Medilab/assets/js/main.js')}}"></script> --}}
+    {{--
+    <script src="assets/js/main.js"></script> --}}
+    {{--
+    <script src="{{ asset('template/Medilab/assets/js/main.js')}}"></script> --}}
 
     <!-- Template Main JS File -->
     <!-- <script src="{{ asset('template/Medilab/assets/js/main/assets.js') }}"></script> -->
     <script src="{{ asset('template/Medilab/assets/js/main.js') }}"></script>
 
     <script>
-    document.addEventListener("DOMContentLoaded", function() {
-      var header = document.getElementById("header");
+        document.addEventListener("DOMContentLoaded", function () {
+            var header = document.getElementById("header");
 
-      window.addEventListener("scroll", function() {
-        // Jika halaman di-scroll ke bawah, tambahkan kelas untuk mengubah latar belakang menjadi putih
-        if (window.scrollY > 0) {
-          header.classList.add("header-solid");
-        } 
-        // Jika halaman di-scroll kembali ke atas, hapus kelas untuk mengembalikan latar belakang transparan
-        else {
-          header.classList.remove("header-solid");
-        }
-      });
-    });
-  </script>
+            window.addEventListener("scroll", function () {
+                // Jika halaman di-scroll ke bawah, tambahkan kelas untuk mengubah latar belakang menjadi putih
+                if (window.scrollY > 0) {
+                    header.classList.add("header-solid");
+                }
+                // Jika halaman di-scroll kembali ke atas, hapus kelas untuk mengembalikan latar belakang transparan
+                else {
+                    header.classList.remove("header-solid");
+                }
+            });
+        });
+    </script>
 
     <script>
         const subsidiaryBtn = document.getElementById('subsidiary-btn');
@@ -443,28 +482,29 @@
         });
     </script>
 
-{{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-<script src="https://cdn.jsdelivr.net/npm/aos@next/dist/aos.js"></script>
+    {{--
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/aos@next/dist/aos.js"></script>
 
-<script>
-    $(document).ready(function() {
-        AOS.init({
-            duration: 1000,  // Durasi animasi dalam milidetik
-            once: true,      // Animasi hanya berjalan sekali
-            mirror: false,   // Animasi saat scrolling ke atas
+    <script>
+        $(document).ready(function () {
+            AOS.init({
+                duration: 1000,  // Durasi animasi dalam milidetik
+                once: true,      // Animasi hanya berjalan sekali
+                mirror: false,   // Animasi saat scrolling ke atas
+            });
         });
-    });
-    $(document).ready(function() {
-        $('.dropdown-toggle').dropdown();
-    });
-</script> --}}
+        $(document).ready(function () {
+            $('.dropdown-toggle').dropdown();
+        });
+    </script> --}}
 
-        <!-- tambahan -->
+    <!-- tambahan -->
     <!-- Template Main JS File -->
     <!-- <script src="assets/js/main.js"></script> -->
-        <!-- end tambahan  -->
+    <!-- end tambahan  -->
 
     <!-- <script> ini chatbot toogle
         window.chatbaseConfig = {
